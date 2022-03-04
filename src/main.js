49138fa7042eac6ed03927c85287bab13e45a318
John Azzinaro <johnazzi430@gmail.com>
2022-03-04 16:30:14 -0500

    added google analytics track

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import 'vuetify/dist/vuetify.min.css'
 import store from './store/index';
+import VueGtag from "vue-gtag";
 
 loadFonts()
 
@@ -12,4 +13,9 @@ createApp(App)
   .use(router)
   .use(vuetify)
   .use(store)
+  .use(VueGtag, {
+    config: { id: "G-C3BP9PBDB0" },
+    params: { anonymize_ip: true },
+    router
+  })
   .mount('#app')
