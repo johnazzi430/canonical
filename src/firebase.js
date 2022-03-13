e772572c56d458cde82526041f8cd4a432410570
John Azzinaro <johnazzi430@gmail.com>
2022-03-13 19:06:09 -0400

    using .env for builds

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-// import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWSF2RKi-MhW6QtxAUjciKN9fpshHo3Kc",
-  authDomain: "canonical-fb93b.firebaseapp.com",
-  projectId: "canonical-fb93b",
-  storageBucket: "canonical-fb93b.appspot.com",
-  messagingSenderId: "703915093257",
-  appId: "1:703915093257:web:e5f386cec7132e297237d8",
-  measurementId: "G-01KEDY9ERR"
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
