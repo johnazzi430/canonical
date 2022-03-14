5b92d8b76644cdbad4ec5b8debe964119074ed84
John Azzinaro <johnazzi430@gmail.com>
2022-03-14 16:27:40 -0400

    added pictures to main

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,65 @@
 <template id="">
-  <div class="text-center center">
-    <v-btn to="/product"
+  <v-container class="flex-container">
+    <div to="/product"
       x-large
-      class="mx-2 big-button"
+      class="item"
       elevation="2"
-      ><h1>Product</h1></v-btn>
-    <v-btn to="/user"
+      @click='$router.push("/product")'
+      >
+      <img class="center img" :src="require('@/assets/images/product.jpg')" alt=""/>
+      <h1 class="center">Product</h1>
+    </div>
+    <div to="/user"
       x-large
-      class="mx-2 big-button"
+      class="item"
       elevation="2"
-      ><h1>Users</h1></v-btn>
-  </div>
+      @click='$router.push("/user")'
+      >
+      <img class="center img" :src="require('@/assets/images/user.jpg')" alt=""/>
+      <h1 class="center">Users</h1>
+    </div>
+  </v-container>
 </template>
 
 <script type="text/javascript">
 
 </script>
 
-<style media="screen" scoped>
+<style scoped lang="scss">
 
-.big-button{
-  width: 250px;
-  height:  250px;
+.flex-container{
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+  justify-content: space-evenly;
+}
+
+.item{
+  flex-grow: 1;
+  width: 300px;
+  height:  300px;
+  padding: 50px;
+  max-width: 300px;
+
+  &:hover{
+      border: 5px teal solid;
+    }
+
+  &:active {
+      background: #D9D9D9;
+    }
+
+}
+
+.img{
+  height: 200px;
+  width: 200px;
 }
 
 .center {
-  margin: auto;
-  margin-top: 88px;
-  width: 100%;
-  padding: 10px;
+  text-align: center;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
